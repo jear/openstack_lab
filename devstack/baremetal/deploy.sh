0da4621a3a6a935b05bd4280c1182ef720cd8911
Uggla <uggla@free.fr>
2016-07-08 16:49:44 +0200

    Add ctrl_ip

--- devstack/baremetal/deploy.sh
+++ devstack/baremetal/deploy.sh
@@ -4,7 +4,7 @@
 
 labnum=$(hostname | sed 's/lab//i')
 
-
+ctrl_ip=$(ip a | grep 10.3.222 | awk '{print $2}' | sed 's#/24##')
 floating_ip_range="172.24.$labnum.0/24"
 public_gw="172.24.$labnum.254"
 floating_pool="start=172.24.$labnum.10,end=172.24.$labnum.100"
