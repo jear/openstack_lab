39f5e10fff99fa39ccca8ca3295b976207622595
Uggla <uggla@free.fr>
2016-07-08 15:18:40 +0200

    Adding missing stuff

--- devstack/baremetal/deploy.sh
+++ devstack/baremetal/deploy.sh
@@ -11,4 +11,6 @@ floating_pool="start=172.24.$labnum.10,end=172.24.$labnum.100"
 labenv="True"
 vrgw="172.24.$labnum.10"
 
+ssh-keyscan -t rsa localhost | cat >> ~/.ssh/known_hosts
+
 ansible-playbook -i inventory -e floating_ip_range=$floating_ip_range -e public_gw=$public_gw -e floating_pool=$floating_pool -e labenv=$labenv -e vrgw=$vrgw baremetal.yml
