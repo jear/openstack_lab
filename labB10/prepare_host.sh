4ed0e5a3cc434abc3549f8a765baeb6dd8b099c0
Uggla <uggla@free.fr>
2016-07-08 11:46:41 +0200

    Review script to deploy on baremetal

--- labB10/prepare_host.sh
+++ labB10/prepare_host.sh
@@ -4,6 +4,32 @@
 # Prepare the lab
 #################
 
+function usage {
+
+echo "Usage: $prog baremetal|virtual"
+echo ""
+echo "baremetal	Openstack controller will be hosted on baremetal"
+echo "virtual		Openstack controller will be hosted on a vagrant(virtualbox) VM"
+exit 0
+}
+
+arg1=$(echo "$1" | tr [:upper:] [:lower:])
+prog=$(basename $0)
+
+case "$1" in
+	"baremetal" )
+	platform=$1
+	;;
+
+	"virtual" )
+	platform=$1
+	;;
+
+	* )
+	usage
+	;;
+esac
+
 # Checking tool deps
 for tool in ssh sshpass pdsh
 do
@@ -33,9 +59,25 @@ then
 fi
 
 # Get the lab bits
-pdsh -R ssh -w ^labhosts.txt "apt-get install -y git ansible vagrant virtualbox pdsh python-glanceclient python-novaclient python-openstackclient"
-pdsh -R ssh -w ^labhosts.txt "vagrant box add ubuntu/trusty64 https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
-pdsh -R ssh -w ^labhosts.txt "git clone https://github.com/uggla/openstack_lab.git"
+if [ "$platform" == "virtual" ]
+then
+	pdsh -R ssh -w ^labhosts.txt "apt-get install -y vagrant virtualbox"
+	pdsh -R ssh -w ^labhosts.txt "vagrant box add ubuntu/trusty64 https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+fi
 
-# Get debian cloud image 
-pdsh -R ssh -w ^labhosts.txt "cd openstack_lab/devstack/images && wget http://cdimage.debian.org/cdimage/openstack/8.5.0/debian-8.5.0-openstack-amd64.qcow2"
+# Add ppa for latest Ansible
+pdsh -R ssh -w ^labhosts.txt "echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >/etc/apt/sources.list.d/ansible.list"
+pdsh -R ssh -w ^labhosts.txt "echo 'deb-src http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main ' >>/etc/apt/sources.list.d/ansible.list"
+pdsh -R ssh -w ^labhosts.txt "gpg --ignore-time-conflict --no-options --no-default-keyring --secret-keyring /etc/apt/secring.gpg --trustdb-name /etc/apt/trustdb.gpg --keyring /etc/apt/trusted.gpg --primary-keyring /etc/apt/trusted.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv 6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367"
+
+pdsh -R ssh -w ^labhosts.txt "apt-get install -y git ansible pdsh python-glanceclient python-novaclient python-openstackclient python-pip libpython2.7-dev"
+pdsh -R ssh -w ^labhosts.txt "git clone https://github.com/uggla/openstack_lab.git"
+pdsh -R ssh -w ^labhosts.txt "pip install -U pip"
+pdsh -R ssh -w ^labhosts.txt "pip install -U pbr"
+pdsh -R ssh -w ^labhosts.txt "pip install -U shade"
+pdsh -R ssh -w ^labhosts.txt "pip install -U os_client_config"
+pdsh -R ssh -w ^labhosts.txt 'sed -ri "\$a export EDITOR=vim" .bashrc'  # Use vim instead of nano
+pdsh -R ssh -w ^labhosts.txt "sed -ri '/bash_completion/,/fi/ s/^#//' .bashrc" # Add bash completion
+
+# Get debian cloud image
+#pdsh -R ssh -w ^labhosts.txt "cd openstack_lab/devstack/images && wget http://cdimage.debian.org/cdimage/openstack/8.5.0/debian-8.5.0-openstack-amd64.qcow2"
