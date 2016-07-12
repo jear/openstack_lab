d6580de78d3ef3365291039f6b86e22d2c9582ee
Uggla <uggla@free.fr>
2016-07-12 13:56:56 +0200

    Add tempo for internalvm

--- bastion/internalvm.sh
+++ bastion/internalvm.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 openstack server create --flavor m1.tiny --image "cirros-0.3.4-x86_64-uec" --key-name bastionkey internalvm
-vmip=$(openstack server show internalvm -f json | jq .addresses | awk '{print $1}' | sed 's/"private=//' | sed 's/,//')
-sleep 30s
+vmip=""
+
+while [ -z "$vmip" ]
+do
+	vmip=$(openstack server show internalvm -f json | jq .addresses | perl -pe 's#.*\s(\d+\.\d+\.\d+\.\d+).*#\1#g')
+	sleep 5s
+done
+
 echo "VM ip $vmip"
