08f58da34f4954d17b024982342adb1971b31012
Uggla <uggla@free.fr>
2016-07-12 13:32:16 +0200

    Add short tempo

--- bastion/internalvm.sh
+++ bastion/internalvm.sh
@@ -2,5 +2,5 @@
 
 openstack server create --flavor m1.tiny --image "cirros-0.3.4-x86_64-uec" internalvm
 vmip=$(openstack server show internalvm -f json | jq .addresses | awk '{print $1}' | sed 's/"private=//' | sed 's/,//')
-
+sleep 5s
 echo "VM ip $vmip"
