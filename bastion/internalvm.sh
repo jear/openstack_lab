0209ebf921780f6a232c57fc6b18c263f575d456
Uggla <uggla@free.fr>
2016-07-12 13:16:59 +0200

    Improve bastion part

---
+++ bastion/internalvm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openstack server create --flavor m1.tiny --image "cirros-0.3.4-x86_64-uec" internalvm
+vmip=$(openstack server show internalvm -f json | jq .addresses | awk '{print $1}' | sed 's/"private=//' | sed 's/,//''"')
+
+echo "VM ip $vmip"
