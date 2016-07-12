0cbbd627db92680146b521b83517214b4ba72775
Uggla <uggla@free.fr>
2016-07-12 13:39:24 +0200

    Add key + extend tempo

--- bastion/internalvm.sh
+++ bastion/internalvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openstack server create --flavor m1.tiny --image "cirros-0.3.4-x86_64-uec" internalvm
+openstack server create --flavor m1.tiny --image "cirros-0.3.4-x86_64-uec" --key-name bastionkey internalvm
 vmip=$(openstack server show internalvm -f json | jq .addresses | awk '{print $1}' | sed 's/"private=//' | sed 's/,//')
-sleep 5s
+sleep 30s
 echo "VM ip $vmip"
