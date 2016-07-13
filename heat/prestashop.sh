b551b9625e87938d8a164152cd766185dd4cad79
Uggla <uggla@free.fr>
2016-07-13 16:49:49 +0200

    More heat stuff

---
+++ heat/prestashop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Prestashop stack
+
+openstack stack create -t presta2.yaml --parameter "key_name=bastionkey;private_net_gateway=10.0.1.1;private_net_name=prestashop_net;private_net_cidr=10.0.1.0/24;private_net_pool_end=10.0.1.100;private_net_pool_start=10.0.1.10;image=Debian Jessie 64-bit;flavor=m1.small;public_net=public" prestashopstack
