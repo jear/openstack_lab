1629c673df0a0d3dd2f70affc60f4782f7f2f2e1
Pierre-Antoine Porte <porte.pierreantoine@gmail.com>
2016-08-02 10:38:02 +0200

    adding /calculate route

--- cloud_native_apps/serviceSlow/server.js
+++ cloud_native_apps/serviceSlow/server.js
@@ -4,7 +4,17 @@ const express = require('express');
 var app       = express();
 const server  = require('http').Server(app);
 
+const responseTime = 5000; // time (milliseconds) to give the answer back on the route /calculate
+
 app.set('port', process.env.PORT | 8080);
+
+app.get('/calculate', (req, res) => {
+  setTimeout(() => {
+    res.status(200).json({
+      number: Math.floor(Math.random()*100)
+    });
+  }, responseTime);
+});
 
 server.listen(app.get('port'), (err) => {
   if (err) throw err;
